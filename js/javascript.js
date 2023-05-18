71ab888e9527576c2531ac0989e95f988130a30b
1945jorge <hamter2010bis@gmail.com>
2023-05-18 10:14:48 -0300

    CLASE_10

---
+++ js/javascript.js
@@ -0,0 +1,22 @@
+
+let nombre = "Jorge Alberto";
+let apellido = "Giardino";
+console.log(nombre+" "+apellido);
+function yo(){
+    let alumno = (nombre + apellido);
+    console.log(nombre + " " + apellido);
+    document.write("Mi nombre es = " + alumno + "<br>");
+}
+yo();
+
+
+let numero1 = 125;
+let numero2 = 45;
+let suma = (numero1 + numero2);
+console.log(suma);
+function sumando(){
+    let total = suma + 150;
+    console.log(total);
+    document.write("La suma es : " + total);
+}
+sumando();
